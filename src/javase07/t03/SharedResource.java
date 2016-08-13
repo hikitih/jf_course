7299564203673cf08dfb48994eb69ad82c6c402a
hikitih <hikitih@yandex.ru>
2016-08-13 18:02:45 +0300

    Add files via upload

---
+++ src/javase07/t03/SharedResource.java
@@ -0,0 +1,36 @@
+package javase07.t03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Goba on 13.08.2016.
+ */
+public class SharedResource {
+
+    private List<Integer> list;
+    private boolean needMoreElements = true;
+
+    public SharedResource() {
+        list = new ArrayList<Integer>();
+    }
+
+    public void setElement(Integer element) {
+        list.add(element);
+    }
+
+    public Integer getELement() {
+        if (list.size() > 0) {
+            return list.remove(0);
+        }
+        return null;
+    }
+
+    public boolean isNeedMoreElements() {
+        return needMoreElements;
+    }
+
+    public void setNeedMoreElements(boolean needMoreElements) {
+        this.needMoreElements = needMoreElements;
+    }
+}
